0a3974b058bf10ceda30c3d84cf824bcbb26a2c2
Sairei <31937866+Sairei@users.noreply.github.com>
2023-12-20 16:26:35 +0000

    Solve day 13 of 2023 (part 2)

--- 2023/day_13/puzzle_day.java
+++ 2023/day_13/puzzle_day.java
@@ -10,8 +10,8 @@ public class PuzzleDay {
 		long res_1 = solvePuzzle1();
 		System.out.println("Result for puzzle 1 = " + res_1);
 
-		// long res_2 = solvePuzzle2();
-		// System.out.println("Result for puzzle 2 = " + res_2);
+		long res_2 = solvePuzzle2();
+		System.out.println("Result for puzzle 2 = " + res_2);
     }
 
 	// Transform file into ArrayList
@@ -36,42 +36,89 @@ public class PuzzleDay {
 		return lines;
 	}
 
-	private static int findMirror(ArrayList<String> pattern) {
+	private static int findMirror(ArrayList<String> pattern, int nbErrorMax) {
 		int mirrorLine = (-1);
 
+		int nbError = 0;
 		int evenOdd = pattern.size() % 2;
 
 		// Start on top
 		int i = 0;
+		int iSave = -1;
+		int jSave = -1;
 		int startIndex = pattern.size() - 1 - evenOdd;
 		for (int j=startIndex; j>i; j--) {
 			String l1 = pattern.get(i);
 			String l2 = pattern.get(j);
 
-			if (l1.equals(l2)) {
-				System.out.println(l1 + "(" + i + ")" + " => " + l2  + "(" + j + ")");
+			nbError += countError(l1.split(""), l2.split(""));
+			if (l1.equals(l2) || nbError <= nbErrorMax) {
+				if (iSave == -1 && jSave == -1) {
+					iSave = i;
+					jSave = j;
+				}
 				if (j == (i+1)) {
-					mirrorLine = j;
-					break;
+					if (nbError == nbErrorMax) { 
+						mirrorLine = j; 
+					} else {
+						i = iSave;
+						iSave = -1;
+						j = jSave;
+						jSave = -1;
+					}
 				} else {
 					i++;
 				}
+			} 
+			else if (nbError > nbErrorMax) {
+				if (i > 0) {
+					i = 0;
+					j++;
+				}
+				iSave = -1;
+				jSave = -1;
+				nbError = 0;
 			}
 		}
 
+		// System.out.println("");
+
+		nbError = 0;
 		i = pattern.size()-1;
+		iSave = -1;
+		jSave = -1;
 		for (int j=evenOdd; j<i; j++) {
 			String l1 = pattern.get(i);
 			String l2 = pattern.get(j);
 
-			if (l1.equals(l2)) {
-				System.out.println(l1 + "(" + i + ")" + " => " + l2  + "(" + j + ")");
+			nbError += countError(l1.split(""), l2.split(""));
+			if (l1.equals(l2) || nbError <= nbErrorMax) {
+				if (iSave == -1 && jSave == -1) {
+					iSave = i;
+					jSave = j;
+				}
+
 				if (j == (i-1)) {
-					mirrorLine = i;
-					break;
+					if (nbError == nbErrorMax) {
+						mirrorLine = i;
+					} else {
+						i = iSave;
+						iSave = -1;
+						j = jSave;
+						jSave = -1;
+					}
 				} else {
 					i--;
 				}
+			}
+			else if (nbError > nbErrorMax) {
+				if (i < pattern.size()-1) {
+					i = pattern.size()-1;
+					j--;
+				}
+				iSave = -1;
+				jSave = -1;
+				nbError = 0;
 			}
 		}
 
@@ -95,6 +142,16 @@ public class PuzzleDay {
 		return rotate;
 	}
 
+	private static int countError(String[] line1, String[] line2) {
+		int nbErr = 0;
+
+		for (int i=0; i<line1.length; i++) {
+			if (!line1[i].equals(line2[i])) { nbErr++; }
+		}
+
+		return nbErr;
+	}
+
 	/********************/
 	/** The first part **/
 	/********************/
@@ -112,17 +169,15 @@ public class PuzzleDay {
 			}
 			
 			if (line.isEmpty() || l == reader.size()-1) {
-				System.out.println("----------------");
-				int mirrorLine = findMirror(pattern);
+				int mirrorLine = findMirror(pattern, 0);
 				if (mirrorLine == -1) {
-					mirrorLine = findMirror(rotatePattern(pattern));
+					mirrorLine = findMirror(rotatePattern(pattern), 0);
 				} else {
 					mirrorLine *= 100;
 				}
 
 				res += mirrorLine;
 				pattern = new ArrayList<>();
-				System.out.println("----------------");
 			}
 		}
 		
@@ -135,7 +190,26 @@ public class PuzzleDay {
 
 	private static long solvePuzzle2() {
 		long res = 0;
-		ArrayList<String> reader = fileToArrayList("data_test.txt");
+		ArrayList<String> reader = fileToArrayList("puzzle_day_13.txt");
+
+		ArrayList<String> pattern = new ArrayList<>();
+		for (int l=0; l<reader.size(); l++) {
+			String line = reader.get(l);
+
+			if (line.length() > 0) {
+				pattern.add(line);
+			}
+			if (line.isEmpty() || l == reader.size()-1) {
+				int mirrorLine = findMirror(rotatePattern(pattern), 1);
+				if (mirrorLine == -1) {
+					mirrorLine = findMirror(pattern, 1);
+					mirrorLine *= 100;
+				} 
+
+				res += mirrorLine;
+				pattern = new ArrayList<>();
+			}
+		}
 		
 		return res;
    	}
