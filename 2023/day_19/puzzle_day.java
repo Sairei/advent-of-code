3b1a3359133d467beebc1dc742ef487f746b5505
Sairei <31937866+Sairei@users.noreply.github.com>
2024-09-02 21:56:08 +0000

    Solve day 19 of 2023 part 2

--- 2023/day_19/puzzle_day.java
+++ 2023/day_19/puzzle_day.java
@@ -14,15 +14,6 @@ public class PuzzleDay {
 			this.name = name;
 			this.rules = new LinkedList<>();
 		}
-
-		public String toString() {
-			String res = name + "{ ";
-			for (Rule r : rules) {
-				 res += r.toString() + " | ";
-			}
-			res = res.substring(0, res.length()-3) + "}";
-			return res;
-		}
 	}
 
 	public static class Rule {
@@ -48,10 +39,6 @@ public class PuzzleDay {
 				return val > Long.parseLong(value);
 			}
 		}
-
-		public String toString() {
-			return categorie + op + value + " => " + to;
-		}
 	}
 
 	public static class Entrie {
@@ -64,25 +51,26 @@ public class PuzzleDay {
 		public Long getCompleteValue() {
 			return map.entrySet().stream().mapToLong(e -> e.getValue()).sum();
 		}
+	}
 
-		public String toString() {
-			String res = "{ ";
-			for (Map.Entry<String, Long> entry : map.entrySet()) {
-				 res += entry.getKey() + "=" + entry.getValue() + " | ";
-			}
-			res = res.substring(0, res.length()-3) + "}";
-			return res;
+	public static class Range {
+		int min, max;
+		public Range(int min, int max) {
+			this.min = min;
+			this.max = max;
 		}
 	}
 
 	public static Map<String, Workflow> workflowMap = new HashMap<>();
 	public static List<Entrie> entriesMap = new ArrayList<>();
+	public static Map<String, Range> rangeMap = new HashMap<>();
+	public static List<Map<String, Range>> validRanges = new ArrayList<>();
     public static void main(String[] args) {
 		long res_1 = solvePuzzle1();
 		System.out.println("Result for puzzle 1 = " + res_1);
 
-		// long res_2 = solvePuzzle2();
-		// System.out.println("Result for puzzle 2 = " + res_2);
+		long res_2 = solvePuzzle2();
+		System.out.println("Result for puzzle 2 = " + res_2);
     }
 
 	// Transform file into ArrayList
@@ -143,7 +131,6 @@ public class PuzzleDay {
 
 		for (Entrie e : entriesMap) {
 			boolean isOk = work(e, workflowMap.get("in"));
-			// System.out.println(isOk + " => " + e);
 			res += isOk ? e.getCompleteValue() : 0;
 		}
 
@@ -152,7 +139,6 @@ public class PuzzleDay {
 
 	public static boolean work(Entrie entrie, Workflow workflow) {
 		boolean res = false;
-		// System.out.print(workflow.name + " => ");
 		for (Rule rule : workflow.rules) {
 			if (rule.categorie.isEmpty()) {
 				if (validateWork(rule.to) != null) {
@@ -176,6 +162,58 @@ public class PuzzleDay {
 		return res;
 	}
 
+	public static long startWorkWithRange() {
+		Map<String, Range> startRangeEntry = new HashMap<>();
+		startRangeEntry.put("x", new Range(1, 4000));
+		startRangeEntry.put("m", new Range(1, 4000));
+		startRangeEntry.put("a", new Range(1, 4000));
+		startRangeEntry.put("s", new Range(1, 4000));
+		
+		workWithRange(startRangeEntry, "in");
+
+		long res = 0;
+		for (Map<String, Range> rangeEntry : validRanges) {
+			long n = 1;
+			for (Map.Entry<String, Range> entry : rangeEntry.entrySet()) {
+				n *= entry.getValue().max - entry.getValue().min + 1;
+			}
+			res += n;
+		}
+		return res;
+	}
+
+	public static void workWithRange(Map<String, Range> rangeEntry, String workflowName) {
+		if (validateWork(workflowName) != null) {
+			if (validateWork(workflowName)) {
+				validRanges.add(rangeEntry);
+			}
+			return;
+		} 
+
+		Workflow workflow = workflowMap.get(workflowName);
+		for (Rule rule : workflow.rules) {
+			if (rule.categorie.isEmpty()) {
+				workWithRange(rangeEntry, rule.to);
+				return;
+			}
+
+			int ruleValue = Integer.parseInt(rule.value);
+			if (rule.op.equals(">")) {
+				Range newRange = new Range(ruleValue + 1, rangeEntry.get(rule.categorie).max);
+				Map<String, Range> newMap = new HashMap<>(rangeEntry);
+				newMap.put(rule.categorie, newRange);
+				workWithRange(newMap, rule.to);
+				rangeEntry.put(rule.categorie, new Range(rangeEntry.get(rule.categorie).min, ruleValue));
+			} else {
+				Range newRange = new Range(rangeEntry.get(rule.categorie).min, ruleValue - 1);
+				Map<String, Range> newMap = new HashMap<>(rangeEntry);
+				newMap.put(rule.categorie, newRange);
+				workWithRange(newMap, rule.to);
+				rangeEntry.put(rule.categorie, new Range(ruleValue, rangeEntry.get(rule.categorie).max));
+			}
+		}
+	}
+
 	public static Boolean validateWork(String to) {
 		Boolean res = null;
 		if (to.equals("A")) {
@@ -192,7 +230,6 @@ public class PuzzleDay {
 
 	private static long solvePuzzle1() {
 		long res = 0;
-		// ArrayList<String> reader = fileToArrayList("data_test.txt");
 		ArrayList<String> reader = fileToArrayList("puzzle_day_19.txt");
 		
 		constructMaps(reader);
@@ -208,6 +245,11 @@ public class PuzzleDay {
 
 	private static long solvePuzzle2() {
 		long res = 0;
+		ArrayList<String> reader = fileToArrayList("puzzle_day_19.txt");
+		
+		constructMaps(reader);
+
+		res = startWorkWithRange();
 
 		return res;
 	}
